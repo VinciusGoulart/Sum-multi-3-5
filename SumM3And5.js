8cc930c99a487dc72e9c429d9158cfcbb3a4c4b9
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-11-28 20:48:41 -0300

    Create verifyNumber, removing unnecessary comment

--- SumM3And5.js
+++ SumM3And5.js
@@ -2,16 +2,18 @@ document.getElementById("result").addEventListener("click", function () {
     const number = document.getElementById("numberInput").value;
     const resultContainer = document.getElementById("resultContainer");
 
-    // Verifique se o número é válido
+    resultContainer.innerHTML = verifyNumber(number);
+});
+
+function verifyNumber(number) {    
     if (number <= 0 || number === "") {
         openErrorModal("Digite um valor positivo maior que 0.");
-        resultContainer.innerHTML = ""; // Limpe o conteúdo do contêiner de resultados
-    } else {
-        // Calcule o resultado e exiba-o
+        return "";
+    } else {        
         const result = sumM3And5(number);
-        resultContainer.innerHTML = `Resultado: ${result}`;
+        return `Resultado: ${result}`;
     }
-});
+}
 
 function openErrorModal(errorMessage) {
     const errorModal = document.getElementById('errorModal');
@@ -20,8 +22,7 @@ function openErrorModal(errorMessage) {
     errorText.textContent = errorMessage;
     errorModal.style.display = 'block';
 
-    // Feche o modal automaticamente após 2 segundos
-    setTimeout(function() {
+    setTimeout(function () {
         errorModal.style.display = 'none';
     }, 2000);
 }
