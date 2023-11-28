c1d288720ab3d1d516d731345f5170ee45cca7fe
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-11-28 17:18:42 -0300

    Created Modal popUp error, upgraded code visibilty

--- SumM3And5.js
+++ SumM3And5.js
@@ -1,18 +1,34 @@
 document.getElementById("result").addEventListener("click", function () {
-    const number = document.getElementById("numberInput");
+    const number = document.getElementById("numberInput").value;
+    const resultContainer = document.getElementById("resultContainer");
 
-    const result = sumM3And5(number.value);
-
-    document.getElementById("resultContainer").innerHTML = result;
+    // Verifique se o número é válido
+    if (number <= 0 || number === "") {
+        openErrorModal("Digite um valor positivo maior que 0.");
+        resultContainer.innerHTML = ""; // Limpe o conteúdo do contêiner de resultados
+    } else {
+        // Calcule o resultado e exiba-o
+        const result = sumM3And5(number);
+        resultContainer.innerHTML = `Resultado: ${result}`;
+    }
 });
+
+function openErrorModal(errorMessage) {
+    const errorModal = document.getElementById('errorModal');
+    const errorText = document.getElementById('errorText');
+
+    errorText.textContent = errorMessage;
+    errorModal.style.display = 'block';
+
+    // Feche o modal automaticamente após 2 segundos
+    setTimeout(function() {
+        errorModal.style.display = 'none';
+    }, 2000);
+}
 
 function sumM3And5(num) {
     let result = 0;
 
-    if (num < 0 || num === "" ){
-        return "Digite um numero maior que 0"
-    }
-    
     for (let i = 0; i < num; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             result = result + i;
