9d5e42ffab0471be1231cd74b235db8c262f8ce3
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-11-28 16:53:44 -0300

    Exception treatment

--- SumM3And5.js
+++ SumM3And5.js
@@ -9,10 +9,10 @@ document.getElementById("result").addEventListener("click", function () {
 function sumM3And5(num) {
     let result = 0;
 
-    if (num < 0) {
-        return "Digite um valor positivo maior que 0";
+    if (num < 0 || num === "" ){
+        return "Digite um numero maior que 0"
     }
-
+    
     for (let i = 0; i < num; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             result = result + i;
