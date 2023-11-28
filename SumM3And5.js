a9dc9a8caf6d97e028528b05ad1a703b5a403845
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-11-28 15:00:53 -0300

    Updated index, style and completed SumM3And5

--- SumM3And5.js
+++ SumM3And5.js
@@ -1,11 +1,24 @@
+
+document.getElementById("result").addEventListener("click", function () {
+    const number = document.getElementById("numberInput");
+
+    const result = sumM3And5(number.value);
+
+    document.getElementById("resultContainer").innerHTML = result;
+});
+
 function sumM3And5(num) {
     let result = 0;
+
+    if (num < 0) {
+        return "Digite um valor positivo maior que 0";
+    }
 
     for (let i = 0; i < num; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             result = result + i;
         }
     }
-    
+
     return result;
 }
